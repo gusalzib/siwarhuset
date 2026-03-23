b80a75464c0830d1142fff7a92cd34ddb5970b6b
ibrahim alzoubi <gusalzib@student.gu.se>
2026-03-23 10:45:48 +0100

    [Config] Create frontend dist folder and relevant code to server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,6 +25,14 @@ app.use('/api/upload', require('./routes/uploadRoutes'));
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+// Serve frontend natively if running in production map
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
