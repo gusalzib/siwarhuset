dc488375359165a2b16961c13ce67b32cfee5424
ibrahim alzoubi <gusalzib@student.gu.se>
2026-03-23 11:09:57 +0100

    [In Progress] minor fix in server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -28,7 +28,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 // Serve frontend natively if running in production map
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
